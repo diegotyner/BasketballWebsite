399774ecf0a40daa58189d974a4e19269ad67c3f
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-07-04 23:11:52 -0700

    Happy w tweaks

--- ReactFolder/src/App.tsx
+++ ReactFolder/src/App.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-import data from './assets/YT_Basketball.json';
 import Table from './components/Table';
 import SearchBox from './components/SearchBox';
 
